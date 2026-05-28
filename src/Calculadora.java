d09fec8d811e968fa03b4be98b78873bedaae69b
Isabella Moreno <imorenom1@unicartagena.edu.co>
2026-05-27 20:39:22 -0400

    Agregar lógica de resta

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -2,4 +2,7 @@ public class Calculadora {
     public double sumar(double numeroUno, double numeroDos) {
         return numeroUno + numeroDos;
     }
+    public double restar(double numeroUno, double numeroDos) {
+        return numeroUno - numeroDos;
+    }
 }
